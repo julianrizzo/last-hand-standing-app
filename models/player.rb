8e74cb4b015886712b395bd360728ff2a5188f5a
Julian Rizzo <Julian@Julians-MBP.home>
2016-07-14 20:45:24 +1000

    redesigned lobby, added colour to player and code display in a banner

--- models/player.rb
+++ models/player.rb
@@ -3,6 +3,8 @@ class Player
   def initialize(name, id)
     @name = name
     @id = id
+
+    @colour = ["#ff7473", "#ede574", "#79bd9a", "#A593E0"].sample
   end
 
   def get_name
@@ -15,6 +17,10 @@ class Player
 
   def get_socket
     return @socket
+  end
+
+  def get_colour
+    return @colour
   end
 
   def add_socket(socket)
