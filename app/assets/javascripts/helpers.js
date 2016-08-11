7394e84a55d3d91a0dcdb01336807dc1c27dcb70
James Formica <james.formica@gmail.com>
2016-08-11 16:33:08 +1000

    i did that thing where i make things crimson

--- app/assets/javascripts/helpers.js
+++ app/assets/javascripts/helpers.js
@@ -3,6 +3,7 @@ function CreateCountdown($element, startingSeconds) {
 
 	var currentSeconds = startingSeconds;
 	var currentMilliSeconds = 0;
+	var inDanger = false;
 
 	var timer = setInterval(function () {
 
@@ -12,6 +13,11 @@ function CreateCountdown($element, startingSeconds) {
 			if (currentMilliSeconds <= 0) {
 				currentMilliSeconds = 60;
 				currentSeconds--;
+			}
+
+			if (!inDanger && currentSeconds <= 3) {
+				inDanger = true;
+				$element.addClass("danger");
 			}
 
 			var milli = currentMilliSeconds < 10 ? "0" + currentMilliSeconds : currentMilliSeconds;
